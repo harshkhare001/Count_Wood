6ad9e718eac71d304b0295ebf16b808a10598833
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2021-01-07 18:30:06 +0530

    Add files via upload

---
+++ addvendorscript.js
@@ -0,0 +1,25 @@
+function addvendor()
+{
+	var username = document.getElementById("Uname").value;
+	var gst = document.getElementById("GSTNO").value;
+	var Address = document.getElementById("add").value;
+	var phone = document.getElementById("num").value;
+
+	if(username==''){
+		alert("customer name required");
+	}
+	else if(gst==''){
+		alert("gst no required");
+	}
+	else if(Address==''){
+		alert("address missing")
+	}
+	else if(phone==''){
+		alert("phone no required");
+	}
+	else{
+		alert('vendor Added sucessfully');
+		window.location.reload();
+	}
+}
+document.querySelector(".butt").addEventListener("click",addvendor);
